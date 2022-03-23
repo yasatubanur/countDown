7643495ecd5c983d8eb39dda26ba274de41f4548
Tuba Nur <yasatubanur@gmail.com>
2022-03-23 23:45:28 +0300

    Timer applied

--- countDown/ViewController.swift
+++ countDown/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var label: UILabel!
+    var timer = Timer()
+    var remainingTime = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        remainingTime = 15
+        label.text = "Time : \(remainingTime)"
     }
 
-
+    
+    @IBAction func startButtonClicked(_ sender: Any) {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerFunc), userInfo: nil, repeats: true)
+    }
+    
+    @objc func timerFunc() {
+        label.text = "Time : \(remainingTime)"
+        remainingTime -= 1
+    }
 }
 
